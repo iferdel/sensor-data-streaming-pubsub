270995e99c86cf6b4e82b3a070213fe9485a333e
iferdel <ignaferdel@gmail.com>
2024-12-08 13:16:58 -0300

    publish with gob encoding+

---
+++ internal/pubsub/publish.go
@@ -0,0 +1,33 @@
+package pubsub
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+    var buffer bytes.Buffer
+    enc := gob.NewEncoder(&buffer)
+    err := enc.Encode(val)
+    if err != nil {
+        return fmt.Errorf("not able to encode value: %v", err)
+    }
+
+    publishMsg := amqp.Publishing{
+        ContentType: "application/gob",
+        Body: buffer.Bytes(),
+    }
+
+    return ch.PublishWithContext(
+        context.Background(),
+        exchange,
+        key,
+        false,
+        false,
+        publishMsg,
+    )
+}
