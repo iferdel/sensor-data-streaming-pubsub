3f20599ad6a886eee4bb8cc6dd995b2495ed1456
iferdel <ignaferdel@gmail.com>
2024-12-12 06:09:35 -0300

    declare and bind
    declare new queue and bind it to exchange with routing key

--- internal/pubsub/consume.go
+++ internal/pubsub/consume.go
@@ -1,5 +1,11 @@
 package pubsub
 
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 type SimpleQueueType int
 
 const (
@@ -14,3 +20,42 @@ const (
 	NackDiscard
 	NackRequeue
 )
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("error while opening channel in declare and bind: %v", err)
+	}
+
+	queue, err := ch.QueueDeclare(
+		queueName,                             // name
+		simpleQueueType == SimpleQueueDurable, // durable
+		simpleQueueType != SimpleQueueDurable, // delete when unused
+		simpleQueueType != SimpleQueueDurable, // exclusive
+		false,                                 // noWait
+		nil,                                   // args
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not declare queue: %v", err)
+	}
+
+	err = ch.QueueBind(
+		queue.Name, // name
+		key,        // routing key
+		exchange,   // exchange
+		false,      // noWait
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not bind queue: %v", err)
+	}
+
+	return ch, queue, nil
+}
