c3e8acd3767beaf4c00402d42d006e695fc486a6
iferdel <ignaferdel@gmail.com>
2024-12-15 15:49:13 -0300

    add: system models

--- internal/routing/models.go
+++ internal/routing/models.go
@@ -2,12 +2,27 @@ package routing
 
 import "time"
 
-type SensorState struct {
-	IsPaused bool
+type SensorStatus struct {
+	SensorName string
+	Status     string
+}
+
+type SensorMeasurement struct {
+	SensorName string
+	Timestamp  time.Time
+	Value      float64
+}
+
+type CommandMessage struct {
+	SensorName string
+	Timestamp  time.Duration
+	Command    string                 // intended for 'sleep' 'resume' 'changeSampleFreq'
+	Params     map[string]interface{} // command specific parameters e.g. {"frequency": 1000}
 }
 
 type SensorLog struct {
 	SensorName  string
 	CurrentTime time.Time
+	Level       string
 	Message     string
 }
