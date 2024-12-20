9b526f180010380ca62f3b58fe641188b4ea9ad5
iferdel <ignaferdel@gmail.com>
2024-12-19 21:02:50 -0300

    awake and sample frequency cmds

--- cmd/server/cmd/awake.go
+++ cmd/server/cmd/awake.go
@@ -1,21 +1,49 @@
 package cmd
 
-//import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
 
-//func main() {
-//   fmt.Println("sending awake command to sensor", "sensorSerialNumber")
-//err := pubsub.PublishGob(
-//    publishCh,                // amqp.Channel
-//    routing.ExchangeTopicIoT, // exchange
-//    fmt.Sprintf(routing.BindKeySensorCommandFormat, sensorSerialNumber), // routing key
-//    routing.CommandMessage{
-//        SensorName: sensorSerialNumber,
-//        Timestamp:  time.Now(),
-//        Command:    "awake",
-//        Params:     nil,
-//    }, // value
-//)
-//if err != nil {
-//    log.Printf("could not publish awake command: %v", err)
-//}
-//}
+	"github.com/iferdel/sensor-data-streaming-server/internal/pubsub"
+	"github.com/iferdel/sensor-data-streaming-server/internal/routing"
+	"github.com/spf13/cobra"
+)
+
+var awakeCmd = &cobra.Command{
+	Use:   "awake",
+	Short: "Awake sensor from sleep and restart generating/sending data",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if conn != nil {
+			defer conn.Close()
+		}
+
+		sensorSerialNumber, err := cmd.Flags().GetString("sensor")
+		if err != nil {
+			log.Printf("error retrieving sensorid flag: %v", err)
+			return
+		}
+
+		fmt.Println("sending awake command to sensor", sensorSerialNumber)
+		err = pubsub.PublishGob(
+			publishCh,                // amqp.Channel
+			routing.ExchangeTopicIoT, // exchange
+			fmt.Sprintf(routing.BindKeySensorCommandFormat, sensorSerialNumber), // routing key
+			routing.CommandMessage{
+				SensorName: sensorSerialNumber,
+				Timestamp:  time.Now(),
+				Command:    "awake",
+				Params:     nil,
+			}, // value
+		)
+		if err != nil {
+			log.Printf("could not publish awake command: %v", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(awakeCmd)
+	awakeCmd.Flags().StringP("sensor", "s", "", "sensorid")
+}
--- cmd/server/cmd/changesamplefrequency.go
+++ cmd/server/cmd/changesamplefrequency.go
@@ -1,29 +1,62 @@
 package cmd
 
-//
-//			if commandInput[1] == "" {
-//				fmt.Println("sample frequency must have an argument for the value")
-//				continue
-//			}
-//			value, err := strconv.Atoi(commandInput[1])
-//			if err != nil {
-//				fmt.Println("sample frequency must be an integer greater than 0")
-//				continue
-//			}
-//			fmt.Println("sending change sample frequency command to sensor", sensorSerialNumber)
-//			err = pubsub.PublishGob(
-//				publishCh,                // amqp.Channel
-//				routing.ExchangeTopicIoT, // exchange
-//				fmt.Sprintf(routing.BindKeySensorCommandFormat, sensorSerialNumber), // routing key
-//				routing.CommandMessage{
-//					SensorName: sensorSerialNumber,
-//					Timestamp:  time.Now(),
-//					Command:    "changeSampleFrequency",
-//					Params: map[string]interface{}{
-//						"sampleFrequency": value,
-//					},
-//				}, // value
-//			)
-//			if err != nil {
-//				log.Printf("could not publish change sample frequency command: %v", err)
-//			}
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/iferdel/sensor-data-streaming-server/internal/pubsub"
+	"github.com/iferdel/sensor-data-streaming-server/internal/routing"
+	"github.com/spf13/cobra"
+)
+
+var changeSampleFrequencyCmd = &cobra.Command{
+	Use:   "changeSampleFrequency",
+	Short: "Change the sample frequency [Hz] of a sensor",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if conn != nil {
+			defer conn.Close()
+		}
+
+		sensorSerialNumber, err := cmd.Flags().GetString("sensor")
+		if err != nil {
+			log.Printf("error retrieving sensorid flag: %v", err)
+			return
+		}
+		newSampleFrequency, err := cmd.Flags().GetString("changeSampleFrequency")
+		if err != nil {
+			log.Printf("error retrieving new sample frequency flag: %v", err)
+			return
+		}
+		value, err := strconv.Atoi(newSampleFrequency)
+		if err != nil {
+			fmt.Println("sample frequency must be an integer greater than 0")
+			return
+		}
+
+		err = pubsub.PublishGob(
+			publishCh,                // amqp.Channel
+			routing.ExchangeTopicIoT, // exchange
+			fmt.Sprintf(routing.BindKeySensorCommandFormat, sensorSerialNumber), // routing key
+			routing.CommandMessage{
+				SensorName: sensorSerialNumber,
+				Timestamp:  time.Now(),
+				Command:    "changeSampleFrequency",
+				Params: map[string]interface{}{
+					"sampleFrequency": value,
+				},
+			}, // value
+		)
+		if err != nil {
+			log.Printf("could not publish change sample frequency command: %v", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(changeSampleFrequencyCmd)
+	changeSampleFrequencyCmd.Flags().StringP("sensor", "s", "", "sensorid")
+	changeSampleFrequencyCmd.Flags().StringP("changeSampleFrequency", "f", "", "sample frequency")
+}
