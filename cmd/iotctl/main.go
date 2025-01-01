fc05bed53fa5473cc1f6c938dae99fbd39f62246
iferdel <ignaferdel@gmail.com>
2024-12-31 19:07:18 -0500

    iotctl service

---
+++ cmd/iotctl/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/iferdel/sensor-data-streaming-server/cmd/iotctl/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- cmd/server/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/iferdel/sensor-data-streaming-server/cmd/server/cmd"
-
-func main() {
-	cmd.Execute()
-}
