8f2eb7638c752eb2c230dd69043ab1fd46c5f81d
iferdel <ignaferdel@gmail.com>
2025-01-02 06:08:40 -0500

    powers up iotctl as it will populate sensor table
    consumes data from sensor after booting for registration. It could give space to use encryption. It also enables the measurement ingestion to be functional

---
+++ cmd/sensor-registry/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// whenever a new sensor is turned on, a registration is realized (auth like).
+	// when successfull (and after being wrote in the database sensor table),
+	// an event is sent to the sensor and this event triggers the start of telemetry measurements.
+}
