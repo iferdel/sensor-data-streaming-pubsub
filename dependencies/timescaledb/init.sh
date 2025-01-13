887a285c0e6cbc0c04be67b9eb65626b86e8b61f
iferdel <ignaferdel@gmail.com>
2025-01-13 20:37:43 -0300

    target+location+constraints+reordering of sql clauses

--- dependencies/timescaledb/init.sh
+++ dependencies/timescaledb/init.sh
@@ -12,7 +12,9 @@ iot_monitoring_password="$IOT_MONITORING_PASSWORD"
 iot_app_password="$IOT_APP_PASSWORD"
 iot_readonly_password="$IOT_READONLY_PASSWORD"
 
-# Create roles, databases and set privileges
+#--------------------------------------------------------------------------------
+# CREATE ROLES, DATABASES AND SET PRIVILEGES
+#--------------------------------------------------------------------------------
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     --------------------------------------------------------------------------------------------
     -- 1. Roles
@@ -89,38 +91,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     -- 3.7 Grant monitor role to iot_monitoring.
     GRANT pg_monitor TO iot_monitoring;
 
-    --------------------------------------------------------------------------------------------
-    -- 4. Create Tables and Constraints.
-    --------------------------------------------------------------------------------------------
-
-    -- 4.1 Connect to the 'iot' database.
-    \connect iot
-
-    -- 4.2 Set role to 'iot_app'.
-    SET ROLE iot_app;
-
-    -- 4.3 Create tables.
-
-    CREATE TABLE sensor (
-	id SERIAL PRIMARY KEY, 
-	serial_number VARCHAR(20) UNIQUE NOT NULL
-	);
-
-    CREATE TABLE sensor_measurement (
-	time TIMESTAMPTZ NOT NULL,
-	sensor_id INTEGER,
-	measurement DOUBLE PRECISION,
-	UNIQUE (time, sensor_id),
-	CONSTRAINT fk_sensor 
-	    FOREIGN KEY (sensor_id) 
-		REFERENCES sensor(id) 
-		    ON DELETE CASCADE
-	);
-    SELECT create_hypertable('sensor_measurement', by_range('time'));
-
-    -- 4.4 Reset role back to initial user
-    RESET ROLE;
-
 EOSQL
 
 #--------------------------------------------------------------------------------
@@ -153,3 +123,69 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     -- ** auto_explain is loaded/enabled by default if used in shared_preload_libraries.
 
 EOSQL
+
+
+#--------------------------------------------------------------------------------
+# CREATE TABLES/SCHEMA
+#--------------------------------------------------------------------------------
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    --------------------------------------------------------------------------------------------
+    -- 1. Create Tables and Constraints.
+    --------------------------------------------------------------------------------------------
+
+    -- 1.1 Connect to the 'iot' database.
+    \connect iot
+
+    -- 1.2 Set role to 'iot_app'.
+    SET ROLE iot_app;
+
+    -- 1.3 Create tables.
+
+    CREATE TABLE target (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
+    );
+
+    CREATE TABLE sensor (
+	id SERIAL PRIMARY KEY, 
+	serial_number VARCHAR(20) UNIQUE NOT NULL,
+	sample_frequency DOUBLE PRECISION CHECK(sample_frequency > 0.0),
+	target_id INTEGER,
+	CONSTRAINT fk_target
+	    FOREIGN KEY (target_id)
+		REFERENCES target(id)
+		    ON DELETE CASCADE
+	);
+	COMMENT ON COLUMN sensor.target_id IS 'one target may have more than 1 sensor, but one sensor is only associated with one target. The sensor id gets populated through new sensors entering the system, but the target_id do not autopopulate on sensors turned on, but afterwards though command-line tooling which would make the association from an user to which target is used for which sensor. This is like a step afterwards since after booting and register serial number, the sensor enters into a --wait-- mode until someone enters the target of the sensor and then this triggers the starting point for start the measurements';
+
+    CREATE TABLE sensor_measurement ( 
+	time TIMESTAMPTZ NOT NULL,
+	sensor_id INTEGER NOT NULL,
+	measurement DOUBLE PRECISION,
+	UNIQUE (time, sensor_id),
+	CONSTRAINT fk_sensor 
+	    FOREIGN KEY (sensor_id) 
+		REFERENCES sensor(id) 
+		    ON DELETE CASCADE
+	);
+	COMMENT ON COLUMN sensor_measurement.measurement IS 'double precision is best for this kind of data since we dont need exact-like precision covered by NUMERIC, as rounding errors can be tolerated';
+    SELECT create_hypertable('sensor_measurement', by_range('time'));
+
+    CREATE TABLE target_location(
+	time TIMESTAMPTZ NOT NULL,
+	target_id INTEGER NOT NULL,
+	location GEOGRAPHY(POINT, 4326),
+	CONSTRAINT fk_target
+	    FOREIGN KEY (target_id)
+		REFERENCES target(id)
+		    ON DELETE CASCADE
+	);
+	COMMENT ON TABLE target_location IS 'multiple sensors may be on the same target, that is why target_location table makes more sense than sensor_location';
+    SELECT create_hypertable('target_location', by_range('time'));
+    CREATE INDEX ON target_location (target_id, time DESC);
+
+    -- 1.4 Reset role back to initial user
+    RESET ROLE;
+
+EOSQL
+
