0837045b5d51507d95d7dc7c8b57e3ffecc36774
iferdel <ignaferdel@gmail.com>
2025-01-15 22:49:04 -0300

    avoid memory layout

--- internal/sensorlogic/sensor.go
+++ internal/sensorlogic/sensor.go
@@ -5,10 +5,10 @@ type Sensor struct {
 }
 
 type SensorState struct {
-	Sensor                    Sensor
-	IsSleep                   bool
-	SampleFrequency           float64
-	SampleFrequencyChangeChan chan float64
+	Sensor                    Sensor       // 16 bytes (string)
+	SampleFrequency           float64      // 8 bytes
+	SampleFrequencyChangeChan chan float64 // 8 bytes
+	IsSleep                   bool         // 1 byte, at the end to avoid memory layout (7 bytes of padding)
 }
 
 func NewSensorState(serialNumber string, SampleFrequency float64) *SensorState {
@@ -16,8 +16,8 @@ func NewSensorState(serialNumber string, SampleFrequency float64) *SensorState {
 		Sensor: Sensor{
 			SerialNumber: serialNumber,
 		},
-		IsSleep:                   false,
 		SampleFrequency:           SampleFrequency,
 		SampleFrequencyChangeChan: make(chan float64, 1),
+		IsSleep:                   false,
 	}
 }
