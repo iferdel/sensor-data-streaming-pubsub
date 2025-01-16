e0802c4b59d742752365f6c414c3e28042879d68
iferdel <ignaferdel@gmail.com>
2025-01-15 22:49:47 -0300

    air for live reloading of api + api health init

--- cmd/iot-api/handlers.go
+++ cmd/iot-api/handlers.go
@@ -1,1 +1,15 @@
 package main
+
+// "encoding/json"
+// "net/http"
+
+// func fetchSensorHandler(w http.ResponseWriter, req *http.Request) error {
+// 	defer req.Body.Close()
+//
+// 	_ = json.NewDecoder(req.Body)
+//
+// 	// use of anonymous structs to unmarshall json data (or to marshall)
+// 	// difference between nested and embedded structs
+//
+// 	return nil
+// }
--- cmd/iot-api/main.go
+++ cmd/iot-api/main.go
@@ -1,4 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var apiSettings struct {
+	secret string
+	dbConn string
+}
+
+type request struct {
+	path string
+}
+
+func handleRequests(reqs <-chan request) {
+	for req := range reqs {
+		handleRequest(req)
+	}
+}
+
+func handleRequest(req request) {
+	fmt.Println("handling request from", req.path)
+}
+
+const PORT = 8080
+
 func main() {
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Addr:              ":8080", // within a container, setting localhost would only enable communication from within the container
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("error in listen and serve: %v", err)
+	}
+
 }
