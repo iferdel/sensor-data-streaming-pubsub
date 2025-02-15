2778cf54c8111908de2f8d9683696a3725488432
iferdel <ignaferdel@gmail.com>
2025-02-14 21:05:19 -0300

    add: enviroment variables for rabbitmq and timescaledb in utils script

--- utils/wait-for-services.sh
+++ utils/wait-for-services.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 # wait-for-rabbitmq-postgresql.sh
 
-until nc -z -v -w30 rabbitmq 5672
+RABBITMQ_HOST=${RABBITMQ_HOST:-rabbitmq}
+RABBITMQ_PORT=${RABBITMQ_PORT:-5672}
+POSTGRES_HOST=${POSTGRES_HOST:-timescaledb}
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+
+until nc -z -v -w30 $RABBITMQ_HOST $RABBITMQ_PORT
 do
   echo "Waiting for RabbitMQ..."
   sleep 1
 done
 echo "RabbitMQ is up and running!"
 
-until nc -z -v -w30 timescaledb 5432
+until nc -z -v -w30 $POSTGRES_HOST $POSTGRES_PORT
 do
   echo "Waiting for PostgreSQL..."
   sleep 1
