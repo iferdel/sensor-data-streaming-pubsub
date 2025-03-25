c7aab65a85a12cfc63dafc3cf5d5d2c93e8b4b68
iferdel <ignaferdel@gmail.com>
2025-03-24 22:33:30 -0300

    add: simulate signal sensor logic with noise random and following normal distribution

--- internal/sensorlogic/sensorsignal.go
+++ internal/sensorlogic/sensorsignal.go
@@ -1,6 +1,9 @@
 package sensorlogic
 
-import "math"
+import (
+	"math"
+	"math/rand/v2"
+)
 
 type SineWave struct {
 	Amplitude float64
@@ -9,5 +12,19 @@ type SineWave struct {
 }
 
 func (sw *SineWave) Generate(dt float64) float64 {
-	return sw.Amplitude * math.Sin(2*math.Pi*sw.Frequency*dt+sw.Phase)
+	return sw.Amplitude * math.Sin(2.0*math.Pi*sw.Frequency*dt+sw.Phase)
+}
+
+func SimulateSignal(waves []SineWave, t float64) float64 {
+
+	// Generate the superimposed signal
+	var value float64
+	for _, wave := range waves {
+		value += wave.Generate(t)
+	}
+
+	noiseAmplitude := rand.Float64()
+	value += (noiseAmplitude * rand.NormFloat64())
+
+	return value
 }
