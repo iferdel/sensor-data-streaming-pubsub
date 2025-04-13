14bf253b9c29a7654d0381a2104a306c170bb5a9
iferdel <ignaferdel@gmail.com>
2025-04-13 11:55:49 -0400

    refactor: application tables into their own .sql file to improve reading and scalability

--- dependencies/timescaledb/init.sh
+++ dependencies/timescaledb/init.sh
@@ -153,120 +153,22 @@ EOSQL
 
 
 #--------------------------------------------------------------------------------
-# CREATE TABLES/SCHEMA iot DATABASE
+# CREATE APPLICATION TABLES iot DATABASE
 #--------------------------------------------------------------------------------
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    --------------------------------------------------------------------------------------------
-    -- 1. Create Tables and Constraints.
-    --------------------------------------------------------------------------------------------
-
-    -- 1.1 Connect to the 'iot' database.
-    \connect iot
-
-    -- 1.2 Set role to 'iot_app'.
-    SET ROLE iot_app;
-
-    -- 1.3 Create tables.
-
-    CREATE TABLE account (
-	id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-	username VARCHAR(50) UNIQUE NOT NULL,
-	password_hash TEXT NOT NULL,
-	created_at TIMESTAMPTZ DEFAULT NOW()
-    );
-
-    CREATE TABLE api_key (
-	id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-	account_id UUID NOT NULL, 
-	api_key_hash TEXT NOT NULL,
-	created_at TIMESTAMPTZ DEFAULT NOW(),
-	expires_at TIMESTAMPTZ DEFAULT NULL,
-	revoked BOOL DEFAULT FALSE,
-	CONSTRAINT fk_account
-	    FOREIGN KEY (account_id)
-		REFERENCES account(id)
-		    ON DELETE CASCADE
-    );
-    COMMENT ON TABLE api_key IS 'one user may have more than one api key depending on the machine that is being logged into the system';
-    CREATE INDEX idx_api_key_hash ON api_key (api_key_hash);
-
-    CREATE TABLE target (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50) NOT NULL
-    );
-
-    CREATE TABLE sensor (
-	id SERIAL PRIMARY KEY, 
-	serial_number VARCHAR(20) UNIQUE NOT NULL,
-	sample_frequency DOUBLE PRECISION CHECK(sample_frequency > 0.0),
-	target_id INTEGER,
-	CONSTRAINT fk_target
-	    FOREIGN KEY (target_id)
-		REFERENCES target(id)
-		    ON DELETE CASCADE
-	);
-    COMMENT ON COLUMN sensor.target_id IS 'one target may have more than 1 sensor, but one sensor is only associated with one target. The sensor id gets populated through new sensors entering the system, but the target_id do not autopopulate on sensors turned on, but afterwards though command-line tooling which would make the association from an user to which target is used for which sensor. This is like a step afterwards since after booting and register serial number, the sensor enters into a --wait-- mode until someone enters the target of the sensor and then this triggers the starting point for start the measurements';
-
-    CREATE TABLE sensor_measurement ( 
-	time TIMESTAMPTZ NOT NULL,
-	sensor_id INTEGER NOT NULL,
-	measurement DOUBLE PRECISION NOT NULL,
-	UNIQUE (time, sensor_id),
-	CONSTRAINT fk_sensor 
-	    FOREIGN KEY (sensor_id) 
-		REFERENCES sensor(id) 
-		    ON DELETE CASCADE
-	);
-    COMMENT ON COLUMN sensor_measurement.measurement IS 'double precision is best for this kind of data since we dont need exact-like precision covered by NUMERIC, as rounding errors can be tolerated';
-    
-    -- If you call SELECT * FROM create_hypertable(...) the return value is formatted as a table with column headings.
-    SELECT * FROM create_hypertable(
-	'sensor_measurement', 
-	'time', 
-	chunk_time_interval=>'5 minutes'::interval
-	);
---    SELECT enable_chunk_skipping('sensor_measurement', 'sensor_id');
---    ALTER TABLE sensor_measurement SET (
---      timescaledb.compress,
---      timescaledb.compress_orderby = 'time DESC',
---      timescaledb.compress_segmentby = 'sensor_id'
---    );
---    SELECT compress_chunk(show_chunks('sensor_measurement'));
---    SELECT add_compression_policy('sensor_measurement', INTERVAL '5 minutes');
-
-    CREATE TABLE target_location(
-	time TIMESTAMPTZ NOT NULL,
-	target_id INTEGER NOT NULL,
-	location GEOGRAPHY(POINT, 4326),
-	CONSTRAINT fk_target
-	    FOREIGN KEY (target_id)
-		REFERENCES target(id)
-		    ON DELETE CASCADE
-	);
-    COMMENT ON TABLE target_location IS 'multiple sensors may be on the same target, that is why target_location table makes more sense than sensor_location. Another reason is that the sample frequency from sensor_measurement is scoped to the domain of that variable';
-    SELECT create_hypertable('target_location', by_range('time'));
-    CREATE INDEX ON target_location (target_id, time DESC);
-
-    -- 1.4 Reset role back to initial user
-    RESET ROLE;
-
-EOSQL
+psql -v on_error_stop=1 --username iot_app -d iot -f /scripts/iot_database_app_setup.sql
 
 #--------------------------------------------------------------------------------
-# create tables/schema monitoring database
+# CREATE HYPERTABLE_SIZE'S TABLE IN IOT DATABASE FOR MONITORING PURPOSES
 #--------------------------------------------------------------------------------
-psql -v on_error_stop=1 --username iot_monitoring -d monitoring -f /scripts/monitoring_database_statements_history_setup.sql
-
-
-#--------------------------------------------------------------------------------
-# create hypertable_size's table in iot database for monitoring purposes (using fdw)
-#--------------------------------------------------------------------------------
-
 psql -v on_error_top=1 --username iot_app -d iot -f /scripts/iot_database_hypertables_size_history_setup.sql
 
-
 #--------------------------------------------------------------------------------
-# create hypertable_size's schema and Foreign Data Wrapper (FDW) as a one-time job
+# ONE-TIME JOB THAT USES FOREIGN DATA WRAPPER TO CONNECT TO IOT DB AN GATHER HYPERTABLE_SIZE TABLE
 #--------------------------------------------------------------------------------
-
 psql -v on_error_top=1 --username iot_monitoring -d monitoring -f /scripts/monitoring_database_fdw_setup.sql
+
+#--------------------------------------------------------------------------------
+# CREATE PG_STAT_STATEMENTS + PG_STAT_KCACHE MONITORING SCHEMA
+#--------------------------------------------------------------------------------
+psql -v on_error_stop=1 --username iot_monitoring -d monitoring -f /scripts/monitoring_database_stat_statements_history_setup.sql
+
---
+++ dependencies/timescaledb/iot_database_app_setup.sql
@@ -0,0 +1,78 @@
+CREATE TABLE account (
+	id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	password_hash TEXT NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT NOW()
+  );
+
+CREATE TABLE api_key (
+	id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+	account_id UUID NOT NULL, 
+	api_key_hash TEXT NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT NOW(),
+	expires_at TIMESTAMPTZ DEFAULT NULL,
+	revoked BOOL DEFAULT FALSE,
+	CONSTRAINT fk_account
+	  FOREIGN KEY (account_id)
+			REFERENCES account(id)
+		    ON DELETE CASCADE
+  );
+  COMMENT ON TABLE api_key IS 'one user may have more than one api key depending on the machine that is being logged into the system';
+  CREATE INDEX idx_api_key_hash ON api_key (api_key_hash);
+
+CREATE TABLE target (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL
+  );
+
+CREATE TABLE sensor (
+	id SERIAL PRIMARY KEY, 
+	serial_number VARCHAR(20) UNIQUE NOT NULL,
+	sample_frequency DOUBLE PRECISION CHECK(sample_frequency > 0.0),
+	target_id INTEGER,
+	CONSTRAINT fk_target
+	  FOREIGN KEY (target_id)
+			REFERENCES target(id)
+		    ON DELETE CASCADE
+	);
+  COMMENT ON COLUMN sensor.target_id IS 'one target may have more than 1 sensor, but one sensor is only associated with one target. The sensor id gets populated through new sensors entering the system, but the target_id do not autopopulate on sensors turned on, but afterwards though command-line tooling which would make the association from an user to which target is used for which sensor. This is like a step afterwards since after booting and register serial number, the sensor enters into a --wait-- mode until someone enters the target of the sensor and then this triggers the starting point for start the measurements';
+
+CREATE TABLE sensor_measurement ( 
+	time TIMESTAMPTZ NOT NULL,
+	sensor_id INTEGER NOT NULL,
+	measurement DOUBLE PRECISION NOT NULL,
+	UNIQUE (time, sensor_id),
+	CONSTRAINT fk_sensor 
+	  FOREIGN KEY (sensor_id) 
+			REFERENCES sensor(id) 
+		    ON DELETE CASCADE
+	);
+  COMMENT ON COLUMN sensor_measurement.measurement IS 'double precision is best for this kind of data since we dont need exact-like precision covered by NUMERIC, as rounding errors can be tolerated';
+    
+-- If you call SELECT * FROM create_hypertable(...) the return value is formatted as a table with column headings.
+SELECT * FROM create_hypertable(
+	'sensor_measurement', 
+	'time', 
+	chunk_time_interval=>'5 minutes'::interval
+	);
+--    SELECT enable_chunk_skipping('sensor_measurement', 'sensor_id');
+--    ALTER TABLE sensor_measurement SET (
+--      timescaledb.compress,
+--      timescaledb.compress_orderby = 'time DESC',
+--      timescaledb.compress_segmentby = 'sensor_id'
+--    );
+--    SELECT compress_chunk(show_chunks('sensor_measurement'));
+--    SELECT add_compression_policy('sensor_measurement', INTERVAL '5 minutes');
+
+CREATE TABLE target_location(
+	time TIMESTAMPTZ NOT NULL,
+	target_id INTEGER NOT NULL,
+	location GEOGRAPHY(POINT, 4326),
+	CONSTRAINT fk_target
+		FOREIGN KEY (target_id)
+			REFERENCES target(id)
+		    ON DELETE CASCADE
+	);
+  COMMENT ON TABLE target_location IS 'multiple sensors may be on the same target, that is why target_location table makes more sense than sensor_location. Another reason is that the sample frequency from sensor_measurement is scoped to the domain of that variable';
+  SELECT create_hypertable('target_location', by_range('time'));
+  CREATE INDEX ON target_location (target_id, time DESC);
