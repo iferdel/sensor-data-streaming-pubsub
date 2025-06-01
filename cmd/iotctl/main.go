545cdda6409678f3a60f10990e0eb1836a4fd00d
iferdel <ignaferdel@gmail.com>
2025-06-01 18:20:20 -0400

    add: iotctl versioning into root Execute

--- cmd/iotctl/main.go
+++ cmd/iotctl/main.go
@@ -1,85 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	_ "embed"
 	"os"
-	"time"
+	"strings"
 
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/iferdel/sensor-data-streaming-server/cmd/iotctl/cmd"
 )
 
 const url = "http://localhost:8080/api/v1/"
 
-type model struct {
-	status int
-	err    error
-}
-
-type errMsg struct {
-	err error
-}
-
-func (e errMsg) Error() string {
-	return e.err.Error()
-}
-
-type statusMsg int
-
-func (m model) healthCheck() tea.Msg {
-
-	c := &http.Client{Timeout: 10 * time.Second}
-	res, err := c.Get(url)
-
-	if err != nil {
-		return errMsg{err}
-	}
-
-	return statusMsg(res.StatusCode)
-}
-
-func (m model) Init() tea.Cmd {
-	return m.healthCheck
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case statusMsg:
-		m.status = int(msg)
-		return m, tea.Quit
-
-	case errMsg:
-		m.err = msg
-		return m, tea.Quit
-
-	case tea.KeyMsg:
-		if msg.Type == tea.KeyCtrlC {
-			return m, tea.Quit
-		}
-	}
-
-	return m, nil
-}
-
-func (m model) View() string {
-
-	if m.err != nil {
-		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
-	}
-
-	s := fmt.Sprintf("Checking %s ... ", url)
-
-	if m.status > 0 {
-		s += fmt.Sprintf("%d %s!", m.status, http.StatusText(m.status))
-	}
-
-	return "\n" + s + "\n\n"
-}
+//go:embed version.txt
+var version string
 
 func main() {
-	_, err := tea.NewProgram(model{}).Run()
+	err := cmd.Execute(strings.Trim(version, "\n"))
 	if err != nil {
-		fmt.Printf("there is an error: %v\n", err)
 		os.Exit(1)
 	}
 }
